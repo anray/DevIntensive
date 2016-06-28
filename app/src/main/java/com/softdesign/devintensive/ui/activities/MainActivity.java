706405f1bacbbb434848d0fe82cd4eebc2b86ca8
anray <anray@tut.by>
2016-06-28 23:57:24 +0300

    Added layou with Collapsing toolbar, and Navigation Drawer. Implemented SharedPrefermeces and EditMode

--- app/src/main/java/com/softdesign/devintensive/ui/activities/MainActivity.java
+++ app/src/main/java/com/softdesign/devintensive/ui/activities/MainActivity.java
@@ -1,21 +1,40 @@
 package com.softdesign.devintensive.ui.activities;
 
-import android.graphics.Color;
-import android.os.StrictMode;
-import android.support.v7.app.AppCompatActivity;
+import android.os.Handler;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.util.StringBuilderPrinter;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 import com.softdesign.devintensive.R;
 import com.softdesign.devintensive.utils.ConstantManager;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends BaseActivity implements View.OnClickListener {
 
     private static final String TAG = ConstantManager.TAG_PREFIX + "Main Activity";
+    private int mCurrentEditMode = 0;
+
+    private ImageView mCall;
+    private CoordinatorLayout mCoordinatorLayout;
+    private Toolbar mToolbar;
+    private DrawerLayout mNavigationDrawer;
+    private FloatingActionButton mFab;
+    private EditText mUserPhone, mUserMail, mUserVk, mUserGit1, mUserGit2, mUserGit3, mUserBio;
+
+    private List<EditText> mUserInfo;
 
 
     @Override
@@ -23,15 +42,63 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d(TAG, "onCreate");
+        mCall = (ImageView) findViewById(R.id.call);
+        mCoordinatorLayout = (CoordinatorLayout) findViewById(R.id.main_coordinator_container);
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        mNavigationDrawer = (DrawerLayout) findViewById(R.id.navigation_drawer);
+        mFab = (FloatingActionButton) findViewById(R.id.fab);
+        mFab.setOnClickListener(this);
+
+        mUserPhone = (EditText) findViewById(R.id.phone_et);
+        mUserMail = (EditText) findViewById(R.id.email_et);
+        mUserVk = (EditText) findViewById(R.id.VK_profile_et);
+        mUserGit1 = (EditText) findViewById(R.id.repository1_et);
+        mUserGit2 = (EditText) findViewById(R.id.repository2_et);
+        mUserGit3 = (EditText) findViewById(R.id.repository3_et);
+        mUserBio = (EditText) findViewById(R.id.aboutMyself_et);
+
+        mUserInfo = new ArrayList<>();
+        mUserInfo.add(mUserPhone);
+        mUserInfo.add(mUserMail);
+        mUserInfo.add(mUserVk);
+        mUserInfo.add(mUserGit1);
+        mUserInfo.add(mUserGit2);
+        mUserInfo.add(mUserGit3);
+        mUserInfo.add(mUserBio);
+
+
+        setupToolbar();
+        setupDrawer();
 
 
 
         if (savedInstanceState == null) {
-            //ни разу не запускалось
+            //активити ни разу не запускалось
+            showSnackbar("активити ни разу не запускалось");
+            showToast("активити ни разу не запускалось");
         } else {
-            //запускалось когда-то
-
+            //активити запускалось когда-то
+            mCurrentEditMode = savedInstanceState.getInt(ConstantManager.EDIT_MODE_KEY, 0);
+            changeEditMode(mCurrentEditMode);
+            showSnackbar("активити запускалось когда-то");
+            showToast("активити запускалось когда-то");
         }
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            mNavigationDrawer.openDrawer(GravityCompat.START);
+        }
+        return super.onContextItemSelected(item);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            mNavigationDrawer.openDrawer(GravityCompat.START);
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
@@ -43,6 +110,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     protected void onResume() {
         super.onResume();
+
         Log.d(TAG, "onResume");
     }
 
@@ -74,13 +142,97 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
-
+            case R.id.fab:
+                showSnackbar("Click");
+                if (mCurrentEditMode == 0) {
+                    changeEditMode(1);
+                    mCurrentEditMode = 1;
+                } else {
+                    changeEditMode(0);
+                    mCurrentEditMode = 0;
+                }
         }
     }
 
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
+        outState.putInt(ConstantManager.EDIT_MODE_KEY, mCurrentEditMode);
 
     }
+
+    public void runWithDelay() {
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //TODO: Выполнить с задержкой
+                hideProgess();
+            }
+        }, 5000);
+
+    }
+
+    private void showSnackbar(String message) {
+        Snackbar.make(mCoordinatorLayout, message, Snackbar.LENGTH_LONG).show();
+
+    }
+
+    private void setupToolbar() {
+        setSupportActionBar(mToolbar);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_menu_black_24dp);
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    private void setupDrawer() {
+        NavigationView navigationView = (NavigationView) findViewById(R.id.navigation_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView
+                .OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+                showSnackbar(item.getTitle().toString());
+                item.setChecked(true);
+                mNavigationDrawer.closeDrawer(GravityCompat.START);
+
+                return false;
+            }
+        });
+    }
+
+    /**
+     * переключает режим редактирования
+     *
+     * @param mode если 1 режим редактирования, если false режим просмотра
+     */
+    private void changeEditMode(int mode) {
+
+        for (EditText userValue : mUserInfo) {
+            if (mode == 1) {
+                mFab.setImageResource(R.drawable.ic_check_black_24dp);
+                userValue.setEnabled(true);
+                userValue.setFocusable(true);
+                userValue.setFocusableInTouchMode(true);
+
+
+            } else {
+                mFab.setImageResource(R.drawable.ic_mode_edit_black_24dp);
+                userValue.setEnabled(false);
+                userValue.setFocusable(false);
+                userValue.setFocusableInTouchMode(false);
+            }
+        }
+
+    }
+
+    private void loadUserInfoValue() {
+
+    }
+
+    private void saveUserInfoValue() {
+
+    }
+
 }
--- app/src/main/java/com/softdesign/devintensive/utils/ConstantManager.java
+++ app/src/main/java/com/softdesign/devintensive/utils/ConstantManager.java
@@ -6,4 +6,5 @@ package com.softdesign.devintensive.utils;
 public interface ConstantManager {
     String TAG_PREFIX = "DEV ";
     String COLOR_MODE_KEY = "COLOR_MODE_KEY";
+    String EDIT_MODE_KEY = "EDIT_MODE_KEY";
 }
