5d0aeb52db420fd9d297b5d26177ddd2256dd50d
anray <anray@tut.by>
2016-07-04 17:35:48 +0300

    Modified sizes calculation of topMenu (now it considers API level <21<=)

--- app/src/main/java/com/softdesign/devintensive/ui/view/behaviours/TopMenuBehavior.java
+++ app/src/main/java/com/softdesign/devintensive/ui/view/behaviours/TopMenuBehavior.java
@@ -16,9 +16,11 @@ import android.widget.LinearLayout;
  */
 public class TopMenuBehavior extends CoordinatorLayout.Behavior<LinearLayout> {
 
-    float oldY = 0;
+
+    private Context mContext;
 
     public TopMenuBehavior(Context context, AttributeSet attrs) {
+        this.mContext = context;
     }
 
     @Override
@@ -31,87 +33,60 @@ public class TopMenuBehavior extends CoordinatorLayout.Behavior<LinearLayout> {
 
         CoordinatorLayout.LayoutParams lp = (CoordinatorLayout.LayoutParams) child.getLayoutParams();
         int pxindp = TypedValue.COMPLEX_UNIT_DIP;
-        int toolbarHeight = ((CollapsingToolbarLayout) ((AppBarLayout) parent.getChildAt(0)).getChildAt(0)).getChildAt(1).getHeight();
-        int heightChange = (int) (dependency.getY()) * 72 / (160) * pxindp;
-
-
-        if (dependency.getY() >= (72 + 64 + 24) * pxindp) {
-            if (heightChange >= toolbarHeight) {
-                lp.height = heightChange;
-            }
-        }
-
-        child.setY(dependency.getY());
-        child.setLayoutParams(lp);
-        dependency.setPadding(dependency.getPaddingLeft(), lp.height, dependency.getPaddingRight(), dependency.getPaddingBottom());
-
-
-/*
-        //START another logic of header
-
-
-        //get resizing step(scale). To set scaling proportion between app bar and rating menu. Usage: height of rating menu.Height() = scale*get(Y)
-        float resizeScale;
 
         LinearLayout ratingMenu = (LinearLayout) parent.getChildAt(1); //rating menu
-        int ratingMenuHeight = ratingMenu.getMinimumHeight();
-        int toolbarHeight = ((CollapsingToolbarLayout) ((AppBarLayout) parent.getChildAt(0)).getChildAt(0)).getChildAt(1).getHeight(); //toolbar size
-        int appbarHeight = parent.getChildAt(0).getHeight();
 
+        float ratingMenuMinimumHeight = ratingMenu.getMinimumHeight();
+        float toolbarHeight = ((CollapsingToolbarLayout) ((AppBarLayout) parent.getChildAt(0)).getChildAt(0)).getChildAt(1).getHeight(); //toolbar size
+        float appbarHeight = parent.getChildAt(0).getHeight();
+        float statusBar = 0;
+        float resizeRatio;
 
-        resizeScale = ratingMenuHeight / ((float) (appbarHeight - toolbarHeight));
+        resizeRatio = (ratingMenuMinimumHeight) / (appbarHeight - toolbarHeight); //0.28
 
+        //get StatusBar height
+        statusBar = getStatusBarHeight();
 
-        //set Y position of rating menu
-        child.setY(dependency.getY());
-
-        //calculate and set height of rating menu
-        int upDelta = (int) (ratingMenu.getHeight() - (oldY - dependency.getY()) * resizeScale); //the
-        int downDelta = (int) (ratingMenu.getHeight() + (dependency.getY() - oldY) * resizeScale);
-
-        if (oldY != 0) {
-            if ((dependency.getY() - oldY) < 0) { //scroll up
-                if (upDelta >= ratingMenu.getMinimumHeight()) {
-                    lp.height = upDelta;
-                } else {
-                    lp.height = ratingMenu.getMinimumHeight();
-                }
-            } else { //scroll down
-                if (upDelta <= ratingMenu.getMinimumHeight() * 2) {
-                    lp.height = downDelta;
-                } else {
-                    lp.height = ratingMenu.getMinimumHeight()*2;
-                }
+        //Logic needed because app bar flows under status bar in API < 21
+        int currentapiVersion = android.os.Build.VERSION.SDK_INT;
+        if (currentapiVersion >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+            //for API>=21, where status bar size is considered while appbar is collapsed
+            if (dependency.getY() < appbarHeight / 2) {
+                lp.height = (int) (ratingMenuMinimumHeight + resizeRatio * (dependency.getY() - statusBar - toolbarHeight));
+            } else {
+                lp.height = (int) (ratingMenuMinimumHeight + resizeRatio * (dependency.getY() - toolbarHeight));
             }
+        } else {
+            //for API<21, where status bar size is NEVER considered
+            lp.height = (int) (ratingMenuMinimumHeight + resizeRatio * (dependency.getY() - toolbarHeight));
         }
+/*
+        Log.d(ConstantManager.TAG_PREFIX, "resizeRatio: " + resizeRatio);
+        Log.d(ConstantManager.TAG_PREFIX, "ratingMenuMinimumHeight: " + ratingMenuMinimumHeight);
+        Log.d(ConstantManager.TAG_PREFIX, "Plashka height: " + lp.height);
+        Log.d(ConstantManager.TAG_PREFIX, "dependency.getY(): " + dependency.getY());
+        Log.d(ConstantManager.TAG_PREFIX, "toolbarHeight: " + toolbarHeight);
+*/
 
-        //
+
+        //set sizes for Views
+        child.setY(dependency.getY());
         child.setLayoutParams(lp);
-
-        //set top padding of Nested Scroll
         dependency.setPadding(dependency.getPaddingLeft(), lp.height, dependency.getPaddingRight(), dependency.getPaddingBottom());
 
-        //Log.d(ConstantManager.TAG_PREFIX, "dependency.getY()+oldY: " + dependency.getY() + " " + oldY);
-        //Log.d(ConstantManager.TAG_PREFIX, "onDependentViewChanged,dependency.getY(): " + dependency.getY());
-        //Log.d(ConstantManager.TAG_PREFIX, "onDependentViewChanged,dependency.getY(): " + dependency.getY());
 
-        Log.d(ConstantManager.TAG_PREFIX, "ratingMenuHeight: " + ratingMenuHeight);
-        Log.d(ConstantManager.TAG_PREFIX, "ratingMenu.getHeight(): " + ratingMenu.getHeight());
-        Log.d(ConstantManager.TAG_PREFIX, "ratingMenu.getMinimumHeight(): " + ratingMenu.getMinimumHeight());
-        //Log.d(ConstantManager.TAG_PREFIX, "appbarHeight: " + appbarHeight);
-        //Log.d(ConstantManager.TAG_PREFIX, "toolbarHeight: " + toolbarHeight);
-        //Log.d(ConstantManager.TAG_PREFIX, "LL sizes: " + parent.getChildAt(1).getMinimumHeight() + " " + parent.getChildAt(1).getHeight());
-        Log.d(ConstantManager.TAG_PREFIX, "resizeScale: " + resizeScale);
 
-        Log.d(ConstantManager.TAG_PREFIX, "onDependentViewChanged,dependency.getTranslationY(): " + dependency.getTranslationY());
-        Log.d(ConstantManager.TAG_PREFIX, "onDependentViewChanged,dependency.getScrollY(): " + dependency.getScrollY());
-
-        oldY = dependency.getY();
-        //END another logic of header
-*/
         return true;
 
     }
 
+public float getStatusBarHeight(){
+    float result = 0;
+    int resourceId = mContext.getResources().getIdentifier("status_bar_height", "dimen", "android");
+    if (resourceId > 0) {
+        result = mContext.getResources().getDimensionPixelSize(resourceId);
+    }
+    return result;
+}
 
 }
